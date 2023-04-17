bbd3909b39efb0cdb4255333ac70ec29f4a76155
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-17 17:16:33 -0400

    adding Postgres DB

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -3,9 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/calvarado2004/go-movies-backend/internal/models"
 	"net/http"
-	"time"
 )
 
 // Home is a simple handler function which writes a response.
@@ -38,52 +36,13 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 
 // AllMovies is a simple handler function which writes a response.
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
-	// get all movies
-	var movies []models.Movie
 
-	releaseDate, err := time.Parse("2006-01-02", "1986-03-07")
+	// get all movies from the database
+	movies, err := app.DB.AllMovies()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-
-	// create one movie
-	highlander := models.Movie{
-		ID:          1,
-		Title:       "Highlander",
-		ReleaseDate: releaseDate,
-		Runtime:     116,
-		MPAARating:  "R",
-		Description: "An immortal Scottish swordsman must confront the last of his immortal opponent, a murderously brutal barbarian who lusts for the fabled Prize.",
-		Genre:       "Action, Adventure, Fantasy, Romance",
-		Image:       "https://m.media-amazon.com/images/M/MV5BMWY4MDg1NmUtMDg1ZC00ODUyLWJkOWQtNWIxMWZhMjRlM2Q1XkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_FMjpg_UX1000_.jpg",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-
-	// add movie to movies
-	movies = append(movies, highlander)
-
-	releaseDate, err = time.Parse("2006-01-02", "1981-06-12")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// create one movie
-	rotla := models.Movie{
-		ID:          1,
-		Title:       "Raiders of the Lost Ark",
-		ReleaseDate: releaseDate,
-		Runtime:     115,
-		MPAARating:  "PG-13",
-		Description: "Raiders of the Lost Ark is a 1981 American action adventure film directed by Steven Spielberg, produced by George Lucas, and starring Harrison Ford. It was the first installment in the Indiana Jones franchise.",
-		Genre:       "Action, Adventure",
-		Image:       "https://image.tmdb.org/t/p/w500/4q2hz2m8hubgvijz8Ez0T2Os2Yv.jpg",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-
-	// add movie to movies
-	movies = append(movies, rotla)
 
 	out, err := json.Marshal(movies)
 	if err != nil {
