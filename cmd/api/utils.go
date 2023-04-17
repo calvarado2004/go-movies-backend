e18d8b0e8efa8ff08f02e2df8c33345db18852d8
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-17 17:36:53 -0400

    Adding helper to read JSON

--- cmd/api/utils.go
+++ cmd/api/utils.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 )
 
@@ -34,5 +36,29 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data any, h
 	}
 
 	return nil
-	
+
+}
+
+// readJSON is a helper function that reads JSON data from the request body.
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+
+	maxBytes := 1024 * 1024 // 1MB
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+
+	dec.DisallowUnknownFields() // this will return an error if the request body contains any fields that aren't defined in the data struct
+
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON object")
+	}
+
+	return nil
 }
