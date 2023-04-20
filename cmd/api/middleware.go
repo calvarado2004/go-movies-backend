c7f8bedbf684fc9ac8745aaa6087fa0d07051907
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-19 22:45:32 -0400

    valid login from backend

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -7,6 +7,7 @@ func (app *application) enableCORS(h http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
